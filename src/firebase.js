426eb28ca5fed703638c51c4c1615612a4296f39
unknown <byadiso@gmail.com>
2026-02-08 13:33:49 +0100

    adding add .env file and en variables

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCB3WbP-Uy5C7nyfBc_GT_ovZ-Sl6w5b_Y",
-  authDomain: "lebwpolish.firebaseapp.com",
-  projectId: "lebwpolish",
-  storageBucket: "lebwpolish.firebasestorage.app",
-  messagingSenderId: "1070474378349",
-  appId: "1:1070474378349:web:f1d44f2289989399585364",
-  measurementId: "G-8B019LVVT8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -19,5 +19,3 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-
-
